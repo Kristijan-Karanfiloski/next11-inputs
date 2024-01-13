ca8ce0e20f6304e4288b278b18a9aac4e758b186
Kristijan Karanfiloski <kikaccc29@gmail.com>
2024-01-13 18:09:13 +0100

    'promenina dropdown '

--- src/components/choose-sport-dropdown/ChooseSportDropdown.jsx
+++ src/components/choose-sport-dropdown/ChooseSportDropdown.jsx
@@ -3,22 +3,20 @@ import "./ChooseSportDropdown.css";
 const ChooseSportDropdown = ({ value, onChange, options }) => {
   return (
     <div>
-      <div className="form-field">
-        <select
-          className="sport"
-          value={value}
-          onChange={(e) => onChange(e.target.value)}
-        >
-          <option disabled={true} value="">
-            Choose Sport
+      <select
+        className="sport-name"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+      >
+        <option disabled={true} value="">
+          Choose Sport
+        </option>
+        {options.map((option, idx) => (
+          <option key={idx} value={option.value}>
+            {option.label}
           </option>
-          {options.map((option, idx) => (
-            <option key={idx} value={option.value}>
-              {option.label}
-            </option>
-          ))}
-        </select>
-      </div>
+        ))}
+      </select>
     </div>
   );
 };
